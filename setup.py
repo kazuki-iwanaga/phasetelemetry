ef4c9993278fa36152fbe935667f5a61d1a85e8f
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2025-07-25 16:30:32 +0000

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='phasetelemetry',
+    version='0.0.0',
+    packages=[
+        'phasetelemetry.api',
+        'phasetelemetry.sdk',
+    ],
+    install_requires=[],
+    license='Apache-2.0',
+)
