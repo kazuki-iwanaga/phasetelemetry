27f23471d6b0d246cac72d2f055b7fede9926fc2
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2025-07-27 06:46:59 +0000

    fix: no filter

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='phasetelemetry',
     version='0.1.0',
-    packages=find_packages(where='phasetelemetry'),
+    packages=find_packages(),
     install_requires=[
         "typing==3.10.0.0; python_version=='2.7'",
     ],
