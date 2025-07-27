668821d4f74d77e98badd4bf403ec49adf962f92
Kazuki Iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2025-07-27 15:52:29 +0900

    chore: set version and add readme (#4)

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='phasetelemetry',
-    version='0.0.0',
-    packages=[
-        'phasetelemetry',
-    ],
+    version='0.1.0',
+    packages=find_packages(),
     install_requires=[
         "typing==3.10.0.0; python_version=='2.7'",
     ],
