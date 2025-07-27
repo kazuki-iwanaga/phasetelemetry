56be09cc937a12895c87933f7f88457af245074a
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2025-07-27 06:43:21 +0000

    fix: use find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='phasetelemetry',
     version='0.1.0',
-    packages=[
-        'phasetelemetry',
-    ],
+    packages=find_packages(where='phasetelemetry'),
     install_requires=[
         "typing==3.10.0.0; python_version=='2.7'",
     ],
